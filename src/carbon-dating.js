769a9d72408eb0f010993102a3a15429ccd1daee
Yliya Pehota <pehotaig@gmail.com>
2020-10-31 15:10:35 +0300

    init: tasks Dream team, Carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,12 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let age = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
+  
+  if (typeof sampleActivity === 'string' && age > 0 && !isNaN(age) && age !== Infinity) { 
+    return age;
+  }else {
+    return false;
+  }
 };
